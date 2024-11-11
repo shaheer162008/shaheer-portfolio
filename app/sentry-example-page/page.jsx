e16d8cf7942d523adbb67eb1158562f0585df10b
Shaheer Yousuf <144364578+shaheer162008@users.noreply.github.com>
2024-11-11 19:32:23 +0500

    Update page.jsx

--- app/sentry-example-page/page.jsx
+++ app/sentry-example-page/page.jsx
@@ -65,7 +65,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://javascript-mastery.sentry.io/issues/?project=4507222371729408">Issues Page</a>.
+          <a href="https://eaab7023ce1466f86cf631f20b1c2e9f@o4508280110383104.ingest.us.sentry.io/4508280112349184">Issues Page</a>.
         </p>
         <p style={{ marginTop: "24px" }}>
           For more information, see{" "}
